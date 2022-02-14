a93976c4c81afb38eeb41fdf138b438fbf417f43
smurzaliev <samat.murzaliev@gmail.com>
2022-02-15 01:22:36 +0600

    Initial Commit

---
+++ TikTokClone-Murzaliev/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TikTokClone-Murzaliev
+//
+//  Created by Samat Murzaliev on 15.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
